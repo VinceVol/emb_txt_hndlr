670a0aefc50e54450fb4832979d57779365b229d
VinceV <vincent.volpone@gentex.com>
2025-10-02 16:13:36 -0400

    made a few changes to try and better encompass different number types

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
 #![no_main]
 
-mod float;
+use num_traits::{Num, Signed, ToPrimitive, Unsigned};
 
 pub enum BufError {
     BufTooSmall,
 }
 
+const BUF_LENGTH: usize = 64;
 struct BufTxt {
-    characters: [u8; 64], //the fixed size bit makes things difficult
+    characters: [u8; BUF_LENGTH], //the fixed size bit makes things difficult
 }
 
 impl BufTxt {
-    fn from_u<T: Copy + Sized>(num: T) -> Self {
-        return BufTxt {
-            characters: [0; 64],
-        };
+    fn from_u<T: ToPrimitive + Unsigned>(num: T) -> Result<Self, BufError> {
+        let mut int_num: u64 = num.to_u64().unwrap();
+        let mut output_buf: [u8; BUF_LENGTH] = [0; BUF_LENGTH];
+        let mut i = BUF_LENGTH - 1;
+
+        while i > 0 {
+            //0x30 is super important otherwise the number shows up as blank lol
+            //hexadecimal conversion is 0x30 = 0
+            output_buf[i] = (int_num % 10) as u8 + 0x30;
+            int_num /= 10;
+            i -= 1;
+        }
+        return Ok(BufTxt {
+            characters: output_buf,
+        });
     }
     fn from_f64(num: f64) -> Self {}
 }
