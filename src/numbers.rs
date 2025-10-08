9471cc37152d2d165e360d4510b9b463ced8b9d1
Vincent Volpone <vincent.volpone@gmail.com>
2025-10-08 07:00:02 -0400

    Still fighting with the unsigned function and properly testing its edge cases lol

--- src/numbers.rs
+++ src/numbers.rs
@@ -1,4 +1,4 @@
-use num_traits::{Float, Num, Signed, Signed, ToPrimitive, Unsigned};
+use num_traits::{Float, Num, Signed, ToPrimitive, Unsigned};
 
 #[derive(PartialEq, Debug, Eq)]
 #[allow(dead_code)]
@@ -48,18 +48,23 @@ impl BufTxt {
     }
     fn from_i<T: ToPrimitive + Signed>(num: T) -> Result<Self, BufError> {
         let mut is_neg: bool = false;
-        let mut pos_num: u64 = 0;
+        let mut pos_num_i: i128 = num.to_i128().unwrap();
         if num.is_negative() {
             is_neg = true;
-            if let Some(u_num) = num.to_u64() {
-                pos_num = u_num;
-            } else {
-                return Err(BufError::num_traits_error);
-            }
+            pos_num_i = pos_num_i * -1;
         }
+        assert_eq!(pos_num_i.is_positive(), true);
+        let pos_num = pos_num_i.to_u64().unwrap();
+        println!("The number sent to unsigned was: {}", pos_num);
         match BufTxt::from_u(pos_num) {
             Err(e) => return Err(e),
             Ok(mut buf_txt) => {
+                println!(
+                    "The unsigned fn returned: {}",
+                    core::str::from_utf8(&buf_txt.characters)
+                        .unwrap()
+                        .replace(" ", "")
+                );
                 if !is_neg {
                     return Ok(buf_txt);
                 }
@@ -74,7 +79,7 @@ impl BufTxt {
 
         return Err(BufError::BufTooSmall);
     }
-    fn from_f<T: ToPrimitive + Float>(num: T) -> Result<Self, BufError> {}
+    // fn from_f<T: ToPrimitive + Float>(num: T) -> Result<Self, BufError> {}
 }
 
 #[cfg(test)]
@@ -92,16 +97,51 @@ mod tests {
             "0"
         );
 
-        let res_max = BufTxt::from_u(std::u128::MAX);
-        assert_eq!(res_max.err().unwrap(), BufError::UnsignedTooLarge);
+        let res_over = BufTxt::from_u(std::u128::MAX);
+        assert_eq!(res_over.err().unwrap(), BufError::UnsignedTooLarge);
 
-        println!("Input: 215");
         let res_random = BufTxt::from_u(215u8).unwrap();
         assert_eq!(
             core::str::from_utf8(&res_random.characters)
                 .unwrap()
                 .replace(" ", ""),
             "215"
+        );
+    }
+
+    #[test]
+    fn test_signed_buf() {
+        let res_0 = BufTxt::from_i(0i8).unwrap();
+        assert_eq!(
+            core::str::from_utf8(&res_0.characters)
+                .unwrap()
+                .replace(" ", ""),
+            "0"
+        );
+
+        let res_over = BufTxt::from_i(std::i128::MAX);
+        match res_over {
+            Ok(res) => println!(
+                "The res_over didn't go over and produced: {:?}",
+                core::str::from_utf8(&res.characters)
+            ),
+            Err(e) => assert_eq!(e, BufError::UnsignedTooLarge),
+        }
+
+        let res_random = BufTxt::from_i(215i16).unwrap();
+        assert_eq!(
+            core::str::from_utf8(&res_random.characters)
+                .unwrap()
+                .replace(" ", ""),
+            "215"
+        );
+
+        let neg_res_random = BufTxt::from_i(-3154i32).unwrap();
+        assert_eq!(
+            core::str::from_utf8(&neg_res_random.characters)
+                .unwrap()
+                .replace(" ", ""),
+            "-3154"
         );
     }
 }
