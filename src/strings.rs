14d0c757611c1975a28263527482e40d091d8f12
VinceV <vincent.volpone@gentex.com>
2025-10-15 08:57:14 -0400

    Got the concat function working for BufTxt, should note that the whole crate is setup to not work with ' ' as u8 and at some point we should shift all that over to a number -- like 1 (u8) that isn't encoded

--- src/strings.rs
+++ src/strings.rs
@@ -2,24 +2,23 @@ use crate::{BUF_LENGTH, BufError, BufTxt};
 
 impl BufTxt {
     pub fn concat(a: BufTxt, b: BufTxt) -> Result<Self, BufError> {
-        let mut new_buf: [u8; BUF_LENGTH] = [0; BUF_LENGTH];
+        let mut new_buf: [u8; BUF_LENGTH] = [' ' as u8; BUF_LENGTH];
         let mut nb_index: usize = 0;
-        for i_a in (0..BUF_LENGTH).rev() {
-            if a.characters[i_a] == 0 {
-                nb_index = i_a;
+        for i_b in (0..BUF_LENGTH).rev() {
+            if b.characters[i_b] == ' ' as u8 {
+                nb_index = i_b;
                 break;
             }
-            new_buf[i_a] = a.characters[i_a];
+            new_buf[i_b] = b.characters[i_b];
         }
 
-        for i_b in (0..BUF_LENGTH).rev() {
-            println!("b_i: {}, {}", i_b, BUF_LENGTH - nb_index - 1);
-            if b.characters[i_b] == 0 {
+        for i_a in (0..BUF_LENGTH).rev() {
+            if a.characters[i_a] == ' ' as u8 {
                 break;
-            } else if i_b < (BUF_LENGTH - nb_index) {
+            } else if i_a < (BUF_LENGTH - nb_index) {
                 return Err(BufError::BufTooSmall);
             }
-            new_buf[i_b - (BUF_LENGTH - nb_index)] = b.characters[i_b];
+            new_buf[i_a - (BUF_LENGTH - nb_index)] = a.characters[i_a];
         }
 
         return Ok(Self {
@@ -35,14 +34,14 @@ mod tests {
 
     #[test]
     fn test_concat() {
-        let buf_a = BufTxt::new("hello ").unwrap();
+        let buf_a = BufTxt::new("hello_").unwrap();
         let buf_b = BufTxt::new("world").unwrap();
         let buf_c = BufTxt::concat(buf_a, buf_b).unwrap();
         assert_eq!(
             core::str::from_utf8(&buf_c.characters)
                 .unwrap()
                 .replace(" ", ""),
-            "hello world"
+            "hello_world"
         );
     }
 }
