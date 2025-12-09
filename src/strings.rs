ede0bdf3a86b8dce5c5ebc1ef4b17e9205284dd0
Vincent Volpone <vincent.volpone@gmail.com>
2025-12-09 06:55:56 -0500

    Still working on strings functions, were off by a couple digits in our indexing, still making progress and simpliying some of the functions

--- src/strings.rs
+++ src/strings.rs
@@ -4,28 +4,33 @@ impl BufTxt {
     pub fn concat(a: BufTxt, b: BufTxt) -> Result<Self, BufError> {
         let mut new_buf: [u8; BUF_LENGTH] = [EMPTY_CELL; BUF_LENGTH];
         let mut nb_index: usize = 0;
-        for i_b in (0..BUF_LENGTH).rev() {
+        for i_b in 0..BUF_LENGTH - 1 {
             new_buf[i_b] = b.characters[i_b];
-            if i_b - 1 > 0 && b.characters[i_b - 1] == EMPTY_CELL {
+            println!(
+                "b.characters[{}] = {}",
+                i_b + 1,
+                core::str::from_utf8(&[b.characters[i_b + 1]]).unwrap()
+            );
+            if b.characters[i_b + 1] == EMPTY_CELL {
                 nb_index = i_b;
                 break;
             }
         }
-
-        for i_a in (0..BUF_LENGTH).rev() {
-            if i_a < (BUF_LENGTH - nb_index) {
-                return Err(BufError::BufTooSmall);
-            }
-
-            new_buf[i_a - (BUF_LENGTH - nb_index)] = a.characters[i_a];
-            if i_a - 1 > 0 && a.characters[i_a - 1] == EMPTY_CELL {
-                break;
-            }
+        println!("nb_index: {}", nb_index);
+        if nb_index == 0 {
+            return Err(BufError::BufTooSmall);
         }
 
-        return Ok(Self {
-            characters: new_buf,
-        });
+        for i_a in 0..BUF_LENGTH - nb_index {
+            new_buf[i_a + nb_index] = a.characters[i_a];
+            println!("new_buf[{}] = a.characterss[{}]", i_a + nb_index, i_a);
+            if a.characters[i_a - 1] == EMPTY_CELL {
+                return Ok(Self {
+                    characters: new_buf,
+                });
+            }
+        }
+        return Err(BufError::BufTooSmall);
     }
     pub fn concat_list(buf_list: &[BufTxt]) -> Result<BufTxt, BufError> {
         let mut new_buf = BufTxt::default();
@@ -77,12 +82,13 @@ mod tests {
         let buf_a = BufTxt::from_str("0").unwrap();
         let buf_b = BufTxt::from_str(":").unwrap();
         let buf_c = BufTxt::concat(buf_a, buf_b).unwrap();
-        assert_eq!(
-            core::str::from_utf8(&buf_c.characters)
-                .unwrap()
-                .replace(" ", ""),
-            "0:"
-        );
+        assert_eq!(buf_c.to_str().unwrap(), "0:");
+        // assert_eq!(
+        //     core::str::from_utf8(&buf_c.characters)
+        //         .unwrap()
+        //         .replace(" ", ""),
+        //     "0:"
+        // );
     }
 
     #[test]
@@ -92,12 +98,13 @@ mod tests {
         let sec = BufTxt::from_str("20").unwrap();
         let colon = BufTxt::from_str(":").unwrap();
         let combined = BufTxt::concat_list(&[hr, colon, min, colon, sec]).unwrap();
-        assert_eq!(
-            core::str::from_utf8(&combined.characters)
-                .unwrap()
-                .replace(" ", ""),
-            "0:2:20"
-        );
+        assert_eq!(combined.to_str().unwrap(), "0:2:20");
+        // assert_eq!(
+        //     core::str::from_utf8(&combined.characters)
+        //         .unwrap()
+        //         .replace(" ", ""),
+        //     "0:2:20"
+        // );
     }
 
     #[test]
